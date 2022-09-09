4f1cd3345ed970f0163c39105919493820823984
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-09 16:36:35 +0700

    Adding Components file :sparkles:

--- src/App.js
+++ src/App.js
@@ -55,6 +55,16 @@ const App = () => {
               <Route path="/editor" element="Editor" />
               <Route path="/calendar" element="Calendar" />
               <Route path="/color-picker" element="ColorPicker" />
+
+              {/* Charts */}
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
             </Routes>
           </div>
         </div>
