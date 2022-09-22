7273ddfabd0b7b8b1740f7b6a4b8df334cf652bf
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-22 10:41:15 +0700

    Adding handleCloseSidebar to Sidebar component :sparkles:

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,6 +10,12 @@ import { useStateContext } from '../contexts/ContextProvider';
 const SideBar = () => {
   const { activeMenu, setActiveMenu } = useStateContext();
 
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
+
   const activeLink =
     'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = `flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2`;
@@ -21,7 +27,7 @@ const SideBar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={() => handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -46,7 +52,7 @@ const SideBar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={() => handleCloseSidebar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
