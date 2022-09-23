d2fe1fa9678f1d3fb15333f637f83325930f27f5
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-23 17:16:03 +0700

    Editing Stacked component :hammer:

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -113,6 +113,9 @@ const Ecommerce = () => {
                 />
               </div>
             </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
           </div>
         </div>
       </div>
