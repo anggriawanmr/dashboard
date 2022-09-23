f6e4a874d8a13f893e2d7063d1f2100cbeddfe0b
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-23 16:10:47 +0700

    Editing SparkLine :hammer:

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -104,6 +104,14 @@ const Ecommerce = () => {
                   color="blue"
                 />
               </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
             </div>
           </div>
         </div>
