335fcc3cefd69d694d3d2e684e0e9d0f7c300798
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-26 10:34:56 +0700

    Editing Orders component :hammer:

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -19,8 +19,32 @@ import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl ">
       <Header title="Orders" category="Page" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
     </div>
   );
 };
